9a8a29349b4b5f5ed7e512d6ea0f8d1ea01e7ef8
Vijay <Vizchan99@gmail.com>
2025-01-26 14:09:27 -0600

    basic structure. Not final

--- tinygrad/runtime/autogen/amd_llvm.py
+++ tinygrad/runtime/autogen/amd_llvm.py
@@ -1,16 +1,20 @@
 import llvm.binding as llvm
-from tinygrad.device import Compiler
+from tinygrad.device import Compiler, CompileError
+from pathlib import Path
+from typing import Optional, List
+import tempfile
+import subprocess
 
 class AMDLLVMCompiler(Compiler):
     def __init__(self, arch: str):
-        self.initialize_llvm()
+        self._initialize_llvm()
         self.arch = arch
-        self.context = llvm.LLVMContext()
+        self.context = llvm.Context()
         self.pm = self._create_pass_manager()
-        self.initialize_amd_target()
+        self._initialize_amd_target()
         super().__init__(f"compiler_hip_{arch}")
     
-    def initialize_llvm(self):
+    def _initialize_llvm(self):
         llvm.initialize()
         llvm.initialize_native_target()
         llvm.initialize_native_asmprinter()
@@ -26,3 +30,50 @@ class AMDLLVMCompiler(Compiler):
             reloc_model="pic",
             code_model="small"
         )
+    
+    def _create_pass_manager(self):
+        pm = llvm.PassManager()
+        pm.add('mem2reg')
+        pm.add('instcombine')
+        pm.add('reassociate')
+        pm.add('amdgpu-promote_alloca')
+        
+        return pm
+    
+    def compile_hip(self,src:str)->bytes:
+        try:
+            llvm_ir = self._hip_to_llvm(src)
+            module = llvm.Module(name="kernel.amd", context=self.context)
+            module.triple = "amdgcn-amd-amdhsa--{self.arch}"
+
+            module.parse_assembly(llvm_ir)
+            self.pm.run(module)
+
+            return self._generate_code_object(module)
+        
+        except Exception as e:
+            raise CompileError(f"Failed to compile HIP code: {e}")
+        
+        finally:
+            module.dispose()
+    
+    def _hip_to_llvm(self, src:str)->str:
+        with tempfile.NamedTemporaryFile(suffix=".hip", mode="w") as src_file:
+            src_file.write(src)
+            src_file.flush()
+        
+        clang_cmd = ["clang++","-x","hip","-target",f"amdgcn-amd-amdhsa--{self.arch}","-emit-llvm","-S","-O3"
+                     "-I/opt/rocm/include",src_file.name]
+        try:
+            result = subprocess.run(clang_cmd, capture_output=True, text=True, check=True)
+            return result.stdout
+        except subprocess.CalledProcessError as e:
+            raise CompileError(f"Clang compilation failed: {e.stderr}")
+    
+    def _generate_code_object(self, module:llvm.Module)->bytes:
+        module.verify()
+        return self.target_machine.emit_object(module)
+    
+    def disassemble(self, lib:bytes):
+        asm = subprocess.check_output(["/opt/rocm/llvm/bin/llvm-objdump", '-d', '-'], input=lib)
+        print('\n'.join([x for x in asm.decode('utf-8').split("\n") if 's_code_end' not in x]))
